ca13bb8ea8304145f0c423955a047247fb25e1c5
JiveDig <jivetheuniversal@gmail.com>
2023-10-09 11:56:14 -0400

    More naming fixes

--- build/columns/index.asset.php
+++ build/columns/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1ed4719b72c6dc25dcf1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e97361688727986107b8');
