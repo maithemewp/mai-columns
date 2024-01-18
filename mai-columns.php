a710f195694445d9d34478ccc3b1f8ad0836b606
JiveDig <jivetheuniversal@gmail.com>
2024-01-18 11:42:15 -0500

    remove uncessesary encoding

--- mai-columns.php
+++ mai-columns.php
@@ -445,9 +445,6 @@ class Mai_Columns_Block {
 		// Modify state.
 		$libxml_previous_state = libxml_use_internal_errors( true );
 
-		// Encode.
-		$html = mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8' );
-
 		// Load the content in the document HTML.
 		$dom->loadHTML( "<div>$html</div>" );
 
